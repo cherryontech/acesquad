b928e38e6ea26d2c139a6cd57bc30de4f6f6259a
Parseluni <deliagaiaglow@gmail.com>
2023-03-08 21:04:21 -0500

    feat create offer/request button/link completed

--- client/DisplayCards.jsx
+++ client/DisplayCards.jsx
@@ -1,9 +1,11 @@
 import cards from "./cardDetails";
 import Card from "./Card";
+import { Link } from "react-router-dom";
 
 const DisplayCards = (cards) => {
   return (
     <div>
+      <Link to={`/submit`} className="offer-request-button"> Create an offer or request </Link>
       <Card />
     </div>
   );
